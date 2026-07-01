852e03aba1ab8ad4dff3b43e857125a40634563e
gitrifatjahanazad <rifatjahanazad@gmail.com>
2026-07-01 10:41:11 +0600

    Update Google Analytics tag to G-WPJS6ZV3L0

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-M9L670TD96" strategy="beforeInteractive" />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-WPJS6ZV3L0" strategy="beforeInteractive" />
         <Script id="ga-init" strategy="beforeInteractive">{`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-M9L670TD96');
+          gtag('config', 'G-WPJS6ZV3L0');
         `}</Script>
       </head>
       <body className={inter.className}>
