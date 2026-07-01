240cba6890483e82a001ec9dd4cf789ef5ae9dfa
gitrifatjahanazad <rifatjahanazad@gmail.com>
2026-07-01 11:02:21 +0600

    Add Google Tag Manager GTM-5QH37ZDT to head and body

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           gtag('js', new Date());
           gtag('config', 'G-WPJS6ZV3L0');
         `}</Script>
+        <Script id="gtm-head" strategy="beforeInteractive">{`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5QH37ZDT');
+        `}</Script>
       </head>
       <body className={inter.className}>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5QH37ZDT"
+            height="0" width="0" style={{ display: "none", visibility: "hidden" }} />
+        </noscript>
         <AuthProvider>
           <CartProvider>
             {children}
